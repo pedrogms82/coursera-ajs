c43e8206181bb068db7266fde737589a4d73d2f4
pedrogms82 <pedrogms82@gmail.com>
2016-10-12 22:19:47 +0200

    act

---
+++ assignment1/js/app.js
@@ -0,0 +1,16 @@
+(function () {
+'use strict';
+
+angular.module('a1', [])
+
+.controller('a1Controller', function ($scope) {
+
+  //$scope.lunch-menu="";
+
+  //$scope.checkMenu = 0; // boton para comprobar items de menu
+
+  var resultado = "OK"; // devoler bien o demasiado
+
+});
+
+})();
